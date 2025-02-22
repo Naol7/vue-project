ad9250c9cc5bcc4074acce296fe8b0c4297df956
unknown <naolgezahegne@gmail.com>
2025-02-22 12:28:40 +0300

    first commit

---
+++ vue-3/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+  
+      const name = ref('Naol');
+      const status = ref('active');
+      const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
+      const toggleStatus = () => {
+        if(status.value === 'active') {
+          status.value = 'pending';
+        } else if(status.value === 'pending') {
+          status.value = 'inactive';
+        } else {
+          status.value = 'active';
+        }
+      }
+    
+  
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status==='active'"> User is active</p>
+  <p v-else-if="status ==='pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+ 
+
+  <button @click="toggleStatus">Change Status </button>
+</template>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
