f8cd653b2cb9328a6fb1c8f78234f35d706ea990
unknown <naolgezahegne@gmail.com>
2025-02-26 06:43:26 +0300

    added basic components with styles using tailwind

--- vue-3/src/App.vue
+++ vue-3/src/App.vue
@@ -1,39 +1,15 @@
 <script setup>
-import { ref } from 'vue';
-  
-      const name = ref('Naol');
-      const status = ref('active');
-      const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
-      const toggleStatus = () => {
-        if(status.value === 'active') {
-          status.value = 'pending';
-        } else if(status.value === 'pending') {
-          status.value = 'inactive';
-        } else {
-          status.value = 'active';
-        }
-      }
-    
-  
+   import Navbar from './components/Navbar.vue';
+   import Hero from './components/Hero.vue';
+   import HomeCards from './components/HomeCards.vue';
+   import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status==='active'"> User is active</p>
-  <p v-else-if="status ==='pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
- 
-
-  <button @click="toggleStatus">Change Status </button>
+    <Navbar />
+    <Hero />
+    <HomeCards />   
+    <JobListings />
 </template>
-
 <style scoped>
-h1 {
-  color: blue;
-}
 </style>
